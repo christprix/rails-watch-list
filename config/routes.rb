db0e35a745738f3d1204c5867295bd3a92a2856c
Christian Stprix <christprix@gmail.com>
2023-07-08 16:36:06 +0900

    can make bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show'
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:index, :new, :create, :delete]
+  end
 end
